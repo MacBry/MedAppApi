3701ad536edc4bce77fec6f2776b4fe9f111f5ea
MacBry <maciej.bryja@wp.pl>
2022-04-05 14:38:58 +0200

    work

--- src/test/java/pl/mac/bry/patient/PatientRepositoryTest.java
+++ src/test/java/pl/mac/bry/patient/PatientRepositoryTest.java
@@ -2,19 +2,22 @@ package pl.mac.bry.patient;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static net.bytebuddy.matcher.ElementMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
@@ -26,41 +29,70 @@ public class PatientRepositoryTest {
     private Patient patient1;
 
     @BeforeEach
-    void setUp(){
+    public void setUp(){
+
         patient = Patient.builder()
-                .id(1L)
+                .id(1)
                 .firstName("testFirstName")
                 .lastName("testLastName")
                 .pesel("testPesel")
                 .build();
         patient1 = Patient.builder()
-                .id(2L)
+                .id(2)
                 .firstName("testFirstName1")
                 .lastName("testLastName1")
                 .pesel("testPesel1")
                 .build();
+        patientRepository.save(patient);
+        patientRepository.save(patient1);
     }
 
     @AfterEach
-    void tearDown() {
-        patient = null;
+    public void tearDown() {
         patientRepository.deleteAll();
+        patient = null;
+        patient1 = null;
     }
 
 
 
     @Test
     void findAllShouldReturnSetOfSizeTwo() {
-        patientRepository.save(patient);
-        patientRepository.save(patient1);
+
         Set<Patient> patients = patientRepository.findAll().stream().collect(Collectors.toSet());
         assertThat(patients.size(),equalTo(2));
     }
 
     @Test
     void givenPatientToAddShouldReturnAddedPatient() {
-        patientRepository.save(patient);
         Patient persistPatient = patientRepository.getById(patient.getId());
         assertThat(persistPatient.getId(), equalTo(1L));
+    }
+
+    @Test
+    void givenPatientIdShouldReturnPatientWithThisId() {
+        Patient persistPatient = patientRepository.getById(patient.getId());
+        assertThat(persistPatient.getId(), equalTo(patient.getId()));
+    }
+
+    @Test
+    void givenPatientPeselShouldReturnPatientWithThisPesel() {
+        Patient persistPatient = patientRepository.findPatientByPesel(patient.getPesel()).get();
+        assertThat(persistPatient.getPesel(), equalTo(patient.getPesel()));
+    }
+
+    @Test
+    void deleteAllPatientShouldReturnEmptyPatientSet() {
+        patientRepository.deleteAll();
+        Set<Patient> patients = patientRepository.findAll().stream().collect(Collectors.toSet());
+        assertThat(patients.size(), equalTo(0));
+    }
+
+    @Test
+    void afterDeletePatientShouldNotFoundThisPatient() {
+
+        patientRepository.delete(patient1);
+        Optional optional = patientRepository.findById(patient1.getId());
+        assertThat(optional.isEmpty(), equalTo(true));
     }
 }
---
+++ src/test/java/pl/mac/bry/patient/PatientServiceTest.java
@@ -0,0 +1,92 @@
+package pl.mac.bry.patient;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class PatientServiceTest {
+
+    @Mock
+    private PatientRepository patientRepository;
+
+    @Mock
+    private PatientDtoMapper patientDtoMapper;
+
+
+    @Autowired
+    @InjectMocks
+    private PatientServiceImp patientServiceImp;
+
+
+    private PatientDto patientDto;
+    private PatientDto patientDto1;
+
+    private Patient patient;
+
+    private List<Patient> patientList;
+
+    @BeforeEach
+    public void setUp () {
+        patientList = new ArrayList<>();
+        patient = Patient.builder()
+                .id(1L)
+                .firstName("testFirstName")
+                .lastName("testLastName")
+                .pesel("testPesel")
+                .build();
+
+        patientDto = PatientDto.builder()
+                .firstName("testFirstName")
+                .lastName("testLastName")
+                .pesel("testPesel")
+                .build();
+        patientDto1 = PatientDto.builder()
+                .firstName("testFirstName1")
+                .lastName("testLastName1")
+                .pesel("testPesel1")
+                .build();
+        patientList.add(patient);
+    }
+
+    @AfterEach
+    public void tearDown () {
+        patientList.remove(patient);
+
+        patientDto = null;
+        patientDto1 = null;
+    }
+
+    @Test
+    void givenPatientToAddShouldReturnAddedPatient () {
+        when(patientRepository.save(any())).thenReturn(patient);
+        patientServiceImp.addPatient(patientDto);
+        verify(patientRepository, times(1)).save(any());
+    }
+
+    @Test
+    void givenGetAllPatientsShouldReturnSetOfAllPatients () {
+        patientRepository.save(patient);
+        when(patientRepository.findAll()).thenReturn(patientList);
+        Set<PatientDto> patientDtoSet = patientServiceImp.getAllPatients();
+        assertThat(patientList.stream().collect(Collectors.toSet()).size(), equalTo(patientDtoSet.size()));
+        verify(patientRepository,times(1)).save(patient);
+        verify(patientRepository, times(1)).findAll();
+    }
+}
